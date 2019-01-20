3792026aac9296f06a2e33af763fbb35582bf8b4
lawga <o.f.bataineh@gmail.com>
2019-01-20 15:55:18 -0500

    Using 7 values of good lanes give better smoothness than 10

--- globals.py
+++ globals.py
@@ -1,4 +1,4 @@
 # Define conversions in x and y from pixels space to meters
 ym_per_pix = 30/720  # meters per pixel in y dimension
 xm_per_pix = 3.7/700  # meters per pixel in x dimension
-N = 10 # N is the number of frames that will be pushed to the stack to avarage some measurments
+N = 7 # N is the number of lane-line data that will be pushed to the stack to avarage some measurments to smooth the detected lane area
